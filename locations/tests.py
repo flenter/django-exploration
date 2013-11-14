2d81846c22c19bd92e9514293751633c860d5e18
Jacco Flenter <github@putpixel.nl>
2013-11-14 20:56:52 +0100

    ...

--- locations/tests.py
+++ locations/tests.py
@@ -42,43 +42,7 @@ class DistanceFilterCase(TestCase):
         result = distance.calc_distance(b, a)
         self.assertEqual(result, 8794.624631776502)
 
-from django.test import TestCase
-
-from django.contrib.gis.measure import D
-from django.contrib.gis.geos import GEOSGeometry
-
-from locations.models import Location
-
-
-class LocationTestCase(TestCase):
-    fixtures = ["data/locations.json"]
-
-    def test_find_cities(self):
-        # not really a test... just seeing if there are two items
-        self.assertEqual(Location.objects.count(), 2)
-
-    def test_distances(self):
-
-        # "Random" point 0,0 latitude/longitude
-        p = GEOSGeometry('POINT(0 0)')
-
-        # Select all locations within the diameter of earth
-        ls = Location.objects.filter(geom__distance_lte=(p, D(km=40000)))
-        self.assertEqual(ls.count(), 2)
-
-        # Select a city less than 6000 km's away (AMS)
-        ls = Location.objects.filter(geom__distance_lte=(p, D(km=6000)))
-        self.assertEqual(ls.count(), 1)
-
-        sf = ls[0]
-
-        # Select a city more than 6000 km's away (SF)
-        ls = Location.objects.filter(geom__distance_gte=(p, D(km=6000)))
-        self.assertEqual(ls.count(), 1)
-
-        ams = ls[0]
-        self.assertNotEqual(sf, ams)
-
+import re
 
 from django.test import LiveServerTestCase
 from selenium.webdriver.firefox.webdriver import WebDriver
@@ -100,5 +64,9 @@ class MySeleniumTests(LiveServerTestCase):
     def test_login(self):
         self.selenium.get('%s%s' % (self.live_server_url, '/locations/'))
         print(dir(self.selenium))
+        src = self.selenium.page_source
+        text_found = re.search(r'text_to_search', src)
+        self.assertFalse(text_found)
+        text_found = re.search(r'Amsterdam', src)
         self.assertTrue(False)
     # self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
