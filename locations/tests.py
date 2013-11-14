fe6b522a0939ddd7ae8f84f5ed11f6cbdfdeae55
Jacco Flenter <github@putpixel.nl>
2013-11-14 20:11:39 +0100

    test with selenium

--- locations/tests.py
+++ locations/tests.py
@@ -41,3 +41,64 @@ class DistanceFilterCase(TestCase):
 
         result = distance.calc_distance(b, a)
         self.assertEqual(result, 8794.624631776502)
+
+from django.test import TestCase
+
+from django.contrib.gis.measure import D
+from django.contrib.gis.geos import GEOSGeometry
+
+from locations.models import Location
+
+
+class LocationTestCase(TestCase):
+    fixtures = ["data/locations.json"]
+
+    def test_find_cities(self):
+        # not really a test... just seeing if there are two items
+        self.assertEqual(Location.objects.count(), 2)
+
+    def test_distances(self):
+
+        # "Random" point 0,0 latitude/longitude
+        p = GEOSGeometry('POINT(0 0)')
+
+        # Select all locations within the diameter of earth
+        ls = Location.objects.filter(geom__distance_lte=(p, D(km=40000)))
+        self.assertEqual(ls.count(), 2)
+
+        # Select a city less than 6000 km's away (AMS)
+        ls = Location.objects.filter(geom__distance_lte=(p, D(km=6000)))
+        self.assertEqual(ls.count(), 1)
+
+        sf = ls[0]
+
+        # Select a city more than 6000 km's away (SF)
+        ls = Location.objects.filter(geom__distance_gte=(p, D(km=6000)))
+        self.assertEqual(ls.count(), 1)
+
+        ams = ls[0]
+        self.assertNotEqual(sf, ams)
+
+
+from django.test import LiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+
+class MySeleniumTests(LiveServerTestCase):
+    fixtures = ["data/locations.json"]
+
+    @classmethod
+    def setUpClass(cls):
+        cls.selenium = WebDriver()
+        super(MySeleniumTests, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(MySeleniumTests, cls).tearDownClass()
+
+    def test_login(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/locations/'))
+        print dir(self.selenium)
+        self.assertTrue(False)
+    # self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
